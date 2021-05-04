59cea61c5867e811ab091ceb37334e55c6ee476a
tomkimberlin <6531977+tomkimberlin@users.noreply.github.com>
2021-05-03 21:33:02 -0400

    tweak dashboard styling

--- dashboard.php
+++ dashboard.php
@@ -8,14 +8,19 @@ $Pages = $Dashboard->getPages();
 <?php require('./nav.php'); ?>
 <main class="container py-3">
   <h1>Dashboard</h1>
-  <p><a class="btn btn-primary mt-2" href="new-page.php">New Page</a></p>
   <section class="row">
-    <h2 class="mb-3">Pages</h2>
     <?php if ($Pages['status']) : ?>
-      <form action="search.php" class="pb-3" method="POST">
-        <label for="search">Search</label>
-        <input type="text" name="search" id="search">
-      </form>
+      <div class="d-flex align-items-center">
+        <div>
+          <p><a class="btn btn-primary" href="new-page.php">New Page</a></p>
+        </div>
+        <div style="padding-left: 1rem;">
+          <form action="search.php" class="pb-3" method="POST">
+            <label for="search">Search</label>
+            <input type="text" name="search" id="search">
+          </form>
+        </div>
+      </div>
       <?php foreach ($Pages['data'] as $row) : ?>
         <div class="col-md">
           <div class="border mb-3">
